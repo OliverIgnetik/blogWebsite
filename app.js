f8e3e750991bf1ea6e9310ea93216ea3526fb1c4
Oliver Ignetik <olli@ozemail.com.au>
2019-07-14 00:26:35 +1000

    Used id of posts to search blogs collection

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const ejs = require("ejs");
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-
 // EJS connection 
 const app = express();
 
@@ -109,16 +108,19 @@ app.get('/compose', (req, res) => {
 
 // get individual posts 
 
-app.get('/posts/:postTitle', function (req, res) {
-  const postTitle = req.params.postTitle;
-  // check for the matching post
-
-  posts.forEach(post => {
-    if (_.lowerCase(postTitle) === _.lowerCase(post.postTitle)) {
-      res.render('post', {
-        post:post
-      });
-    }
+app.get('/posts/:id', function (req, res) {
+  const id = req.params.id;
+  
+  // check through the blogs by id
+  Blog.findById( id, (err, post) => {
+     if(err){
+         console.log(`Error: ` + err)
+     } else{
+      //  render post
+        res.render('post', {
+          post: post,
+        });
+     }
   });
 });
 
@@ -137,16 +139,9 @@ app.post('/compose', (req, res) => {
 
   // insert blog into blogDB object 
   blog.save();
-  
-  // blog object for posts array 
-  const post = {
-    postTitle: postTitle,
-    postContent: postContent
-  };
 
-  // posts.push(post);
-
-  res.redirect('/');
+  // redirect to the home page
+  res.redirect('/compose');
 });
 
 // listen for server
